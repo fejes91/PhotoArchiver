b4ec7a86deec010097985b73c2b34697896b4c51
fejes91 <fejes91@gmail.com>
2014-03-02 10:06:15 +0100

    netbeans test commit

--- src/photoarchiverjava/Main.java
+++ src/photoarchiverjava/Main.java
@@ -24,6 +24,10 @@ public class Main {
             for (Tag tag : directory.getTags()) {
                 System.out.println(tag);
             }
+            
+            
+            
+            
         }
 
     }
